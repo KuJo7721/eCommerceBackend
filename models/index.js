1855344bb0d70e97b86d4978396abadc9577ba8c
KJ Krueger <kjkrueger12@gmail.com>
2022-10-11 16:48:42 -0700

    finished association

--- models/index.js
+++ models/index.js
@@ -5,13 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+ foreignKey: 'category_id',
+ onDelete: 'cCASCADE',
+}):
 
 // Categories have many Products
+Category.hasMany(Produce, {
+  foreignKey: 'category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+foreignKey: 'product_id'
+through: ProductTag 
+} )
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id'
+  through: ProductTag 
+})
 module.exports = {
   Product,
   Category,
