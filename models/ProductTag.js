33200a1fac689e479f8d77aee2574ffe262124b6
KJ Krueger <kjkrueger12@gmail.com>
2022-10-11 16:27:28 -0700

    completed product tag table

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,7 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:
+    type: Datatypes.INTEGER,
+    allownull: false,
+    primarykey: true,
+    autoincrement: true,
   },
+  product_id: {
+    type: Datatypes.INTEGER,
+    reference: {
+     model: 'product',
+     key: 'id'
+    }
+  tag_id: {
+    type: Datatypes.INTEGER,
+    reference: {
+     model: 'tag',
+     key: 'id'
+    }
+
+  }
   {
     sequelize,
     timestamps: false,
